3836328d297b8c5fb40d15e472b3deaf478ceb6d
Fabien V <fabien.vermeiren@gmail.com>
2022-05-17 16:10:12 +0200

    setup and connect to cloud DB

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DB_PASS);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log("DB connections successful");
+  });
 const app = require("./app");
 
 const port = process.env.PORT || 3000;
