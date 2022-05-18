f8892fe2e2d1d548094779544f8f9da0d8eae228
Fabien V <fabien.vermeiren@gmail.com>
2022-05-18 16:59:32 +0200

    query middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -8,6 +8,9 @@ const postSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: {
+      type: String,
+    },
     description: {
       type: String,
       required: [true, "a post must have a title"],
@@ -37,6 +40,10 @@ const postSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    secretPost: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -50,7 +57,17 @@ const postSchema = new mongoose.Schema(
 // })
 
 //document middleware: runs before .save() and .create()
-postSchema.pre("save", function () {});
+postSchema.pre("save", function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
+
+//QUERY middleware
+postSchema.pre("/^find/", function (next) {
+  this.find({ secretPost: { $ne: true } });
+  next();
+});
+
 const Post = mongoose.model("Post", postSchema);
 
 module.exports = Post;
